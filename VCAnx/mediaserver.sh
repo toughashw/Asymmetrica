21999f71a702ce5d0b9c62a64aaf7e3c4d1ad2f1
Alessandro Carucci <alessandrocarucci.ac@gmail.com>
2024-02-08 11:49:28 +0000

    errors final push edge-computing

--- VCAnx/mediaserver.sh
+++ VCAnx/mediaserver.sh
@@ -1,18 +1,18 @@
 sudo mkdir VCAnx
 cd /home/asymmetrica/VCAnx/
 
-sudo wget https://updates.networkoptix.com/metavms/5.1.2.37996/arm/metavms-client-5.1.2.37996-linux_arm64.deb
-sudo wget https://updates.networkoptix.com/metavms/5.1.2.37996/arm/metavms-server-5.1.2.37996-linux_arm64.deb
+sudo wget https://updates.networkoptix.com/default/5.1.2.37996/arm/nxwitness-client-5.1.2.37996-linux_arm64.deb
+sudo wget https://updates.networkoptix.com/default/5.1.2.37996/arm/nxwitness-server-5.1.2.37996-linux_arm64.deb
 
-sudo dpkg -i metavms-client-5.1.2.37996-linux_arm64.deb
-sudo dpkg -i metavms-server-5.1.2.37996-linux_arm64.deb
+sudo dpkg -i nxwitness-client-5.1.2.37996-linux_arm64.deb
+sudo dpkg -i nxwitness-server-5.1.2.37996-linux_arm64.deb
 
 sudo systemctl daemon-reload 
 
-sudo systemctl enable networkoptix-metavms-mediaserver.service
-sudo systemctl start networkoptix-metavms-mediaserver.service
-sudo systemctl status networkoptix-metavms-mediaserver.service
+sudo systemctl enable networkoptix-mediaserver.service
+sudo systemctl start networkoptix-mediaserver.service
+sudo systemctl status networkoptix-mediaserver.service
 
-sudo systemctl enable networkoptix-metavms-root-tool.service
-sudo systemctl start networkoptix-metavms-root-tool.service
-sudo systemctl status networkoptix-metavms-root-tool.service
+sudo systemctl enable networkoptix-root-tool.service
+sudo systemctl start networkoptix-root-tool.service
+sudo systemctl status networkoptix-root-tool.service
